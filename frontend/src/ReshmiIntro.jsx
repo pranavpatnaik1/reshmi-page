932686f7d590e9db4b31191710f88e74b1f6c458
Pranav Patnaik <137661401+iamPranavPatnaik@users.noreply.github.com>
2025-06-01 02:01:49 -0400

    feat: imgs

--- frontend/src/ReshmiIntro.jsx
+++ frontend/src/ReshmiIntro.jsx
@@ -7,6 +7,10 @@ export default function ReshmiIntro() {
     <>
       {/* Intro (100vh high, circles + name) */}
       <div className="container">
+        {/* Side Images */}
+        <img src="/reshmileft.png" alt="" className="side-image left" />
+        <img src="/reshmiright.png" alt="" className="side-image right" />
+
         {/* Circles */}
         <div className="circle left-circle" />
         <div className="circle right-circle" />
@@ -19,14 +23,29 @@ export default function ReshmiIntro() {
         </div>
       </div>
 
-      {/* Image Carousel */}
-      <ImageCarousel />
+      {/* Curved Separator */}
+      <div className="curve-separator">
+        <div className="curve-decoration left">
+          <div className="curve-circle"></div>
+          <div className="curve-line"></div>
+        </div>
+        <div className="curve-decoration right">
+          <div className="curve-circle"></div>
+          <div className="curve-line"></div>
+        </div>
+      </div>
 
-      {/* Portfolio Section */}
-      <div className="portfolio-section">
-        <div className="portfolio-content">
-          <div className="portfolio-title">welcome to my shop.</div>
-          <div className="subtitle">we're buzzing to meet you!</div>
+      {/* Wrapper for all content below curve */}
+      <div className="bottom-content-wrapper">
+        {/* Image Carousel */}
+        <ImageCarousel />
+
+        {/* Portfolio Section */}
+        <div className="portfolio-section">
+          <div className="portfolio-content">
+            <div className="portfolio-title">welcome to my shop.</div>
+            <div className="subtitle">we're buzzing to meet you!</div>
+          </div>
         </div>
       </div>
     </>
